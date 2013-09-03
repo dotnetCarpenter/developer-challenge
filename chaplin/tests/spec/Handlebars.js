a96d56841a76756124533f0c0c21ec6dd2cf6263
dotnetCarpenter <jon.ronnenberg@gmail.com>
2013-09-03 20:27:25 +0200

    Handlebars.helpers.select is tested

--- chaplin/tests/spec/Handlebars.js
+++ chaplin/tests/spec/Handlebars.js
@@ -5,8 +5,26 @@ define(['handlebars', '../../js/lib/view-helper'], function(Handlebars) {
 console.log("Custom Handlebars Helpers", Handlebars.helpers);
 
     describe( 'The "select" markup helper', function() {
-        it ( 'should be registered', function() {
+        it( 'should be registered', function() {
             expect( helpers.select ).toBeDefined();
+        });
+
+        it( 'should return an empty <select> if no arguments', function() {
+          var expected = "<select></select>";
+          var actual = helpers.select();
+          expect( actual ).toEqual( expected );
+        });
+
+        it( 'should return a <select> with exactly 1 <option> if given 1 pair', function() {
+          var expected = "<select><option value='Hej'>Bo</option></select>";
+          var actual = helpers.select([[ "Bo", "Hej"]]);
+          expect( actual ).toEqual( expected );
+        });
+
+        it( 'should return a <select> with exactly 3 <option> if given 3 pairs', function() {
+          var expected = "<select><option value='Hej'>Bo</option><option value='Hej'>Bo</option><option value='Hej'>æøå</option></select>";
+          var actual = helpers.select([[ "Bo", "Hej"], [ "Bo", "Hej"], [ "æøå", "Hej"]]);
+          expect( actual ).toEqual( expected );
         });
     });
   });
