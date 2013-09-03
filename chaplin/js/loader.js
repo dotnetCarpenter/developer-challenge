e434b87bbabdc9ffc5d39e1f41dcef9ea9574e1a
dotnetCarpenter <jon.ronnenberg@gmail.com>
2013-09-03 20:00:48 +0200

    refactored require.js config to loader.js

---
+++ chaplin/js/loader.js
@@ -0,0 +1,36 @@
+// Configure the AMD module loader
+requirejs.config({
+  // The path where your JavaScripts are located
+  baseUrl: '.',
+  // Specify the paths of vendor libraries
+  paths: {
+    jquery: '../bower_components/jquery/jquery',
+    underscore: '../bower_components/lodash/lodash',
+    backbone: '../bower_components/backbone/backbone',
+    handlebars: '../bower_components/handlebars/handlebars',
+    text: '../bower_components/requirejs-text/text',
+    chaplin: '../bower_components/chaplin/chaplin'
+  },
+  // Underscore and Backbone are not AMD-capable per default,
+  // so we need to use the AMD wrapping of RequireJS
+  shim: {
+    underscore: {
+      exports: '_'
+    },
+    backbone: {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    },
+    handlebars: {
+      exports: 'Handlebars'
+    }
+  }
+  // For easier development, disable browser caching
+  // Of course, this should be removed in a production environment
+  //, urlArgs: 'bust=' +  (new Date()).getTime()
+});
+
+// Bootstrap the application
+require(['application', 'routes'], function(Application, routes) {
+  new Application({routes: routes, controllerSuffix: '-controller'});
+});
