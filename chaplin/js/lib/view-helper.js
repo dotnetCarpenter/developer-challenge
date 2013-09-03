5349708b32073aadafcfc8862b4b90aa560d3fac
dotnetCarpenter <jon.ronnenberg@gmail.com>
2013-09-03 17:06:39 +0200

    added select helper for handlebars

---
+++ chaplin/js/lib/view-helper.js
@@ -0,0 +1,25 @@
+define([
+  'handlebars',
+  'chaplin',
+  'lib/utils'
+], function(Handlebars, Chaplin, utils) {
+  'use strict';
+
+  // Application-specific Handlebars helpers
+  // -------------------------------------------
+
+  // Get Chaplin-declared named routes. {{#url "like" "105"}}{{/url}}.
+  Handlebars.registerHelper('url', function(routeName) {
+    var params = [].slice.call(arguments, 1);
+    var options = params.pop();
+    return Chaplin.helpers.reverse(routeName, params);
+  });
+
+  // Create select box
+  Handlebars.registerHelper('select', function(pairs, options) {
+    return "<select>" + _(pairs).map(function(pair) {
+      console.log(pair)
+      return "<option value='" + pair[1] + "'>" + pair[0] + "</option>";
+    }).join('') + "</select>";
+  });
+});
